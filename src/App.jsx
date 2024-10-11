149b1a3a76b75a051883da0fa172ef8db1a182f2
Ana Ferreira <anapaulaferreira0309@gmail.com>
2024-10-11 10:56:55 -0300

    add components and pages to the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
-import './App.css'
+import {Header, Footer, Spotlight} from './components';
+import {About, Projects} from './pages';
 
 function App() {
-
   return (
     <>
-      <main>
-        <h1>Seja bem vindo! :)</h1>
-        <p>Desafio de apresentação Pessoal do Projeto Frontend fusion</p>
-      </main>
+      <Router>
+        <div className="w-full h-screen lg:max-w-7xl md:max-w-3xl relative flex flex-col lg:flex-row gap-y-0 items-center text-base p-5 lg:py-12">
+          <div className="min-h-[30vh] w-full h-full lg:min-w-[30vw] lg:max-h-[70vh] gap-5 flex flex-col lg:justify-between p-5">
+            <Header />
+            <Footer />
+          </div>
+
+
+          <div className='w-full h-full min-h-[70vh] lg:max-h-[70vh] overflow-y-auto no-scrollbar'>
+            <Routes>
+              <Route path="/" exact element={<About />} />
+              <Route path="about" element={<About />} />
+              <Route path="projects" element={<Projects />} />
+              <Route path="*" element={<h1>404: page not found</h1>} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+      <Spotlight />
     </>
   );
 }
 
-export default App
+export default App;
