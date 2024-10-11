f46a2f898255a3a7d9cd4e2d239dee36f86f4eda
Ana Ferreira <anapaulaferreira0309@gmail.com>
2024-10-11 10:54:19 -0300

    create About.jsx page

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+export function About() {
+  return (
+    <div id="about" className="h-full flex flex-col gap-5 items-center justify-center">
+      <p>
+        Desenvolvedora em busca de novas oportunidades e formada em Análise e Desenvolvimento de Sistemas em 2021.
+      </p>
+      <p>
+        Após um breve período trabalhando em outra área, retornei meu foco para a tecnologia e possuo forte interesse em &quot;desenvolvimento web full-stack&quot;.
+      </p>
+      <p>
+        Busco uma oportunidade para aplicar meus conhecimentos e habilidades em um ambiente inovador, onde possa contribuir para o desenvolvimento de soluções tecnológicas. Motivada a aplicar meus conhecimentos em projetos desafiadores e contribuir para o crescimento de equipes dinâmicas.
+      </p>
+    </div>
+  );
+}
