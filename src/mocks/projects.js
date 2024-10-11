2f6f1d81eb92f9393de145f9739d95e8ad8d6973
Ana Ferreira <anapaulaferreira0309@gmail.com>
2024-10-11 10:53:51 -0300

    create mocks folder and projects.js file

---
+++ src/mocks/projects.js
@@ -0,0 +1,60 @@
+const path = '/images';
+
+export const projects = [
+  {
+    name: 'WaiterApp',
+    imagePath: `${path}/waiterapp.png`,
+    description: 'O Waiter App é uma aplicação desenvolvida para melhorar o fluxo de pedidos em um restaurante. Ele inclui um app mobile para os garçons criarem pedidos, uma versão web com dashboard para a cozinha controlar os pedidos e uma API para conectar e centralizar os dados entre as duas versões.',
+    techs: ['React', 'TypeScript', 'React Native', 'Vite', 'Styled Components', 'Express'],
+    url: 'https://github.com/ana-ferreiramg/waiterapp'
+  },
+  {
+    name: 'DoctorCare',
+    imagePath: `${path}/doctorcare.png`,
+    description: 'DoctorCare é uma landing page responsiva e personalizável para a plataforma de consultas médicas.',
+    techs: ['HTML', 'CSS', 'JavaScript'],
+    url: 'https://github.com/ana-ferreiramg/doctor-care'
+  },
+  {
+    name: 'Be The Hero',
+    imagePath: `${path}/bethehero.png`,
+    description: 'O projeto visa conectar pessoas que desejam fazer contribuições monetárias a ONG\'s (Organizações não governamentais) que precisam de ajuda.',
+    techs: ['React', 'React Native', 'Express', 'Knex', 'Jest'],
+    url: 'https://github.com/ana-ferreiramg/be-the-hero'
+  },
+  {
+    name: 'La Casa Pizzaria Delivery',
+    imagePath: `${path}/lacasapizzariadelivery.png`,
+    description: 'Landing page simples para uma pizzaria local.',
+    techs: ['HTML', 'CSS', 'JavaScript'],
+    url: 'https://lacasapizzariadelivery.netlify.app/'
+  },
+  {
+    name: 'Ecoleta',
+    imagePath: `${path}/ecoleta.png`,
+    description: 'Site de criação e pesquisa de pontos de coleta de resíduos para reciclagem',
+    techs: ['HTML', 'CSS', 'JavaScript'],
+    url: 'https://github.com/ana-ferreiramg/nlw-ecoleta'
+  },
+  {
+    name: 'Café e Conversa',
+    imagePath: `${path}/cafeeconversa.png`,
+    description: 'Landing page para uma panificadora.',
+    techs: ['React', 'Node.js'],
+    url: 'https://cafe-e-conversa.netlify.app/'
+  },
+  {
+    name: 'Pomodoro',
+    imagePath: `${path}/pomodoro.png`,
+    description: 'Pomodoro simples',
+    techs: ['HTML', 'CSS', 'JavaScript'],
+    url: 'https://github.com/ana-ferreiramg/Pomodoro'
+  },
+  {
+    name: 'Jokempo',
+    imagePath: `${path}/jokempo.png`,
+    description: 'Jogo simples de pedra, papel e tesoura.',
+    techs: ['HTML', 'CSS', 'JavaScript'],
+    url: 'https://github.com/ana-ferreiramg/jokempo'
+  },
+];
